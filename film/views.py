f2eda2025f3d6549d1cc08c6b545d322dc6e2c5b
jrco1989 <jrco1989@hotmail.com>
2020-01-17 14:43:36 -0500

    correcion de errores

--- film/views.py
+++ film/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 
-def create_film(request)
+
+"""def create_film(request):
     if request.method == 'POST':
         title= request.POST['title']
 		director= request.POST['director']
         opening_text= request.POST['openning_text']
         save.film()
-        return redirect('home')
+   """    
+       
+       
+def home(request):
+    return render (request=request, template_name="home.html")
 
 
         
--- start_wars/urls.py
+++ start_wars/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
+from film import views
 
 urlpatterns = [
     path('admin/', 
         admin.site.urls),
-    path('',
-        TemplateView.as_view(template_name='home.html'),
-        name='home', )
-    #path(route='ingreso/',view= views.ingreso, name='ingreso'),
+    #path('',
+     #   TemplateView.as_view(template_name='home.html'),
+      #  name='home', )
+    path(route='',view= views.home, name='home'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
