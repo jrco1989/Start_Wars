91457af60288825c7f357d3867c7581721a0e02b
jrco1989 <jrco1989@hotmail.com>
2020-01-17 13:08:32 -0500

    añadiendo la primera vista, url y configuracion para archivos estáticos

--- film/views.py
+++ film/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def create_film(request)
+    if request.method == 'POST':
+        title= request.POST['title']
+		director= request.POST['director']
+        opening_text= request.POST['openning_text']
+        save.film()
+        return redirect('home')
+
+
+        
